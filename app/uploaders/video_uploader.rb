0d955d2cb96d32ae766ee431c964463cafa23185
VladimirMozgin <mozgin.vova@gmail.com>
2016-06-19 23:45:02 +0300

    add view for tour presentation uploader

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -38,7 +38,7 @@ class VideoUploader < BaseUploader
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+
   def extension_whitelist
     %w(mp4 avi ogg)
   end
